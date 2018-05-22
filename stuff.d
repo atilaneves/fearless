d1e775a1a30e1884bd090583fbc9d2e6fbbadba3
Atila Neves <atila.neves@gmail.com>
2018-05-22 22:23:39 +0200

    Refactoring

--- stuff.d
+++ stuff.d
@@ -1,5 +1,75 @@
 import std.concurrency: Tid;
 
+void main() @safe {
+    import std.stdio: writeln;
+    import std.concurrency: spawn, send, receiveOnly, thisTid;
+
+    auto s = shared Shared!int(42);
+
+    {
+        scope i = s.lock();
+        // writeln is @system for some reason
+        () @trusted { writeln("i: ", *i); }();
+        *i = 33;
+        () @trusted { writeln("i: ", *i); }();
+    }
+
+    auto tid = () @trusted { return spawn(&func, thisTid); }();
+    () @trusted { tid.send(&s); }();
+    () @trusted { receiveOnly!bool; }();
+    () @trusted { writeln("i: ", *s.lock); }();
+}
+
+
+void func(Tid tid) @trusted { // Both receive and send are @system
+    import std.concurrency: receive, send;
+
+    receive(
+        // ref shared(Shared!int) didn't work
+        (shared(Shared!int)* m) {
+            auto i = m.lock;
+            *i = ++*i;
+        },
+    );
+
+    tid.send(true);
+}
+
+
+struct Shared(T) {
+
+    private T _payload;
+    private Mutex _mutex;
+
+    this(A...)(auto ref A args) shared {
+        import std.functional: forward;
+        this._mutex = shared Mutex(null /*attr*/);
+        this._payload = T(forward!args);
+    }
+
+    // non-static didn't work - weird error messages
+    static struct Guard {
+
+        private shared T* _payload;
+        private shared Mutex* _mutex;
+
+        alias payload this;
+
+        scope T* payload() @trusted {
+            return cast(T*) _payload;
+        }
+
+        ~this() scope {
+            _mutex.unlock_nothrow();
+        }
+    }
+
+    auto lock() shared @trusted {
+        _mutex.lock_nothrow;
+        return Guard(&_payload, &_mutex);
+    }
+}
+
 // Can't use core.sync.mutex due to the member functions not being `scope`
 static struct Mutex {
 
@@ -17,86 +87,11 @@ static struct Mutex {
         pthread_mutex_destroy(cast(pthread_mutex_t*)&_mutex);
     }
 
-    void lock_nothrow() shared scope nothrow {
+    void lock_nothrow() @trusted shared scope nothrow {
         pthread_mutex_lock(cast(pthread_mutex_t*)&_mutex);
     }
 
-    void unlock_nothrow() shared scope nothrow {
+    void unlock_nothrow() @trusted shared scope nothrow {
         pthread_mutex_unlock(cast(pthread_mutex_t*)&_mutex);
     }
-}
-
-struct Shared(T) {
-
-    // Can't use core.sync.mutex due to the member functions not being `scope`
-    //import core.sync.mutex: Mutex;
-
-    private T _payload;
-    private Mutex _mutex;
-
-    this(A...)(auto ref A args) shared {
-        import std.functional: forward;
-
-        //this._mutex = new shared Mutex();
-        this._mutex = shared Mutex(null /*attr*/);
-        this._payload = T(forward!args);
-    }
-
-    static struct Guard {
-
-        private shared T* _payload;
-        private shared Mutex* _mutex;
-
-        alias payload this;
-
-        scope T* payload() @trusted {
-            return cast(T*)_payload;
-        }
-
-        ~this() scope @trusted  {
-            _mutex.unlock_nothrow();
-        }
-    }
-
-    auto lock() shared @trusted {
-        _mutex.lock_nothrow;
-        return Guard(&_payload, &_mutex);
-    }
-}
-
-
-void main() @safe {
-    import std.stdio: writeln;
-    import std.concurrency: spawn, send, receiveOnly, thisTid;
-
-    auto s = shared Shared!int(42);
-
-    {
-        scope i = s.lock();
-        *i = 33;
-        // writeln here is @system for some reason
-        () @trusted { writeln("i: ", *i); }();
-        (*i)++;
-        () @trusted { writeln("i: ", *i); }();
-    }
-
-    auto tid = () @trusted { return spawn(&func, thisTid); }();
-    () @trusted { tid.send(&s); }();
-    () @trusted { receiveOnly!bool; }();
-    () @trusted { writeln("i is now ", *s.lock); }();
-}
-
-
-// Both receive and send are @system
-void func(Tid tid) @trusted {
-    import std.concurrency: receive, send;
-
-    receive(
-        (shared(Shared!int)* m) {
-            auto i = m.lock;
-            *i = ++*i;
-        },
-    );
-
-    tid.send(true);
 }
